5e6ff7fc4d8e73c40996891c4a6b297eab6a9866
modreness <zan.andjic@gmail.com>
2022-12-22 01:10:55 +0100

    Update ga4forwp.php

--- ga4forwp.php
+++ ga4forwp.php
@@ -1,33 +1,38 @@
 <?php
 /*
-Plugin Name: Google Analytics 4 for WP
-Plugin URI: https://tehnoklik.ba/ga4forwp
-Description: Connects to Google Analytics 4 and allows the administrator to enter the tracking ID.
+Plugin Name: Basic Google Analytics 4 for WP
+Plugin URI: https://plugins.tehnoklik.ba/basic-google-analytics-4-for-wp
+Description: Basic and lightweight plugin which connects to Google Analytics 4 and allows the administrators of website to enter the tracking ID.
 Version: 1.0
 Author: Žan Anđić, Tehnoklik
 Author URI: https://tehnoklik.ba
 License: GPL2
 */
+
+//Require file with functions to register options page and menu item
+
 require_once plugin_dir_path( __FILE__ ) . 'ga4-admin.php';
+
+//Require file with function and HTML form (input field) where user can enter tracking ID
+
 require_once plugin_dir_path( __FILE__ ) . 'ga4-admin-form.php';
+
+//showing tracking ID in gtag snippet in head section of website
 
 function ga4forwp_tracking_code() {
   $tracking_id = get_option( 'ga4forwp_tracking_id' );
   if ( ! empty( $tracking_id ) ) {
-    ?>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $tracking_id; ?>"></script>
-    <script>
+    $src = 'https://www.googletagmanager.com/gtag/js?id=' . esc_html( $tracking_id );
+    wp_enqueue_script( 'ga4forwp-gtag', esc_url( $src ), array(), null, false );
+
+    $script = "
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
 
-      gtag('config', '<?php echo $tracking_id; ?>');
-    </script>
-    <?php
+      gtag('config', '" . esc_js( $tracking_id ) . "');
+    ";
+    wp_add_inline_script( 'ga4forwp-gtag', $script, 'after' );
   }
 }
-add_action( 'wp_head', 'ga4forwp_tracking_code' );
-
-
-
+add_action( 'wp_enqueue_scripts', 'ga4forwp_tracking_code' );
